2a6507f3adaef84ae8640ac674a2bf783338989e
Letícia Pardini <pardini448@gmail.com>
2023-05-28 13:55:29 -0300

    fix: error return delete times

--- src/utils/validate/validateCreateNewRestaurant.ts
+++ src/utils/validate/validateCreateNewRestaurant.ts
@@ -7,9 +7,10 @@ export const linkSchemaCreateRestaurant = yup.object({
     city: yup.string().required("City is required"),
     type: yup.string().required("Type is required"),
     password: yup.string().required("password is required"),
-    cellphone: yup.string().required("Type is required"),
-    socialMidea: yup.string().required("Type is required"),
-    number: yup.number().required("Type is required"),
+    cellphone: yup.string().required("Cell is required"),
+    socialMidea: yup.string(),
+    link: yup.string(),
+    number: yup.number().required("Number is required"),
   })
 });
 
